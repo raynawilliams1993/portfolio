a7d373e29a57007adf154f0af67704c7bcb07fb2
rayna williams <Raynawilliams123@gmail.com>
2020-05-09 15:17:12 -0600

    linked all my projects and repos to the buttons on my portfolio page

--- src/components/pages/portfolio.js
+++ src/components/pages/portfolio.js
@@ -22,7 +22,7 @@ const Example = (props) => {
       <Card>
         <h1> Technical Skills </h1>
         <p>
-        JavaScript, MERN, MySql, NoSql, HTML, CSS, Es6, Handlebars, API, AJAX, AWS and Node.Js. 
+          JavaScript, MERN, MySql, NoSql, HTML, CSS, GitHub, Git, Es6, Handlebars, API, AJAX, AWS and Node.Js.
 
         </p>
       </Card>
@@ -30,55 +30,52 @@ const Example = (props) => {
         <Card>
           <CardImg top width="100%" src={pic1} alt="Fitness App" />
           <CardBody>
-            <CardTitle>Card title</CardTitle>
+            <CardTitle>Fitness Tracker</CardTitle>
             <CardSubtitle>Card subtitle</CardSubtitle>
             <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-            <Button>Button</Button>
+            <Button href="https://github.com/raynawilliams1993/fitnessTracker">GitHub</Button>
+            <Button href="https://agile-sands-15513.herokuapp.com/exercise?id=5e4b0f904df7e300171b940d">View App</Button>
           </CardBody>
         </Card>
-        <Card>
-          <CardImg top width="100%" src={pic2} alt="Employee Directory App" />
-          <CardBody>
-            <CardTitle>Card title</CardTitle>
-            <CardSubtitle>Card subtitle</CardSubtitle>
-            <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-            <Button>Button</Button>
-          </CardBody>
-        </Card>
+     
         <Card>
           <CardImg top width="100%" src={pic3} alt="Burger Handlebars App" />
           <CardBody>
-            <CardTitle>Card title</CardTitle>
+            <CardTitle>Burger!</CardTitle>
             <CardSubtitle>Card subtitle</CardSubtitle>
             <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-            <Button>Button</Button>
+            <Button href=" https://github.com/raynawilliams1993/burger">GitHub</Button>
+            <Button href="https://eatingburgers2424.herokuapp.com/">View App</Button>
           </CardBody>
         </Card>
         <Card>
           <CardImg top width="100%" src={pic4} alt="Project 01-3rdpartyAPIs" />
           <CardBody>
-            <CardTitle>Card title</CardTitle>
+            <CardTitle>Project 1: Bored in Denver?</CardTitle>
             <CardSubtitle>Card subtitle</CardSubtitle>
             <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-            <Button>Button</Button>
+            <Button href="https://github.com/7thact/Project01">GitHub</Button>
+            <Button href=" https://7thact.github.io/Project01/">View App</Button>
           </CardBody>
         </Card>
         <Card>
           <CardImg top width="100%" src={pic5} alt="Budget Tracker App" />
           <CardBody>
-            <CardTitle>Card title</CardTitle>
+            <CardTitle>Progressive Budget Tracker</CardTitle>
             <CardSubtitle>Card subtitle</CardSubtitle>
             <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-            <Button>Button</Button>
+            <Button href=" https://boiling-escarpment-16918.herokuapp.com/">GitHub</Button>
+            <Button href=" https://github.com/raynawilliams1993/budgetTracker">View App</Button>
           </CardBody>
         </Card>
         <Card>
           <CardImg top width="100%" src={pic6} alt="Final Project-MERNSTACK" />
           <CardBody>
-            <CardTitle>Card title</CardTitle>
+            <CardTitle>Project 3: Fantasy Basketball</CardTitle>
             <CardSubtitle>Card subtitle</CardSubtitle>
             <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-            <Button>Button</Button>
+            <Button href="https://github.com/raynawilliams1993/ballerz">GitHub</Button>
+            <Button href=" https://ballerz24.herokuapp.com/">View App</Button>
           </CardBody>
         </Card>
       </CardDeck>
